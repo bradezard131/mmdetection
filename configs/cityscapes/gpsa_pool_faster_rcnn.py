0bfaab8ed9da6c4f5a73d75adb32d6c10fad4766
Brad Ezard <bradley.ezard@postgrad.curtin.edu.au>
2022-10-14 10:37:36 +0800

    conf updates

--- configs/cityscapes/gpsa_pool_faster_rcnn.py
+++ configs/cityscapes/gpsa_pool_faster_rcnn.py
@@ -2,10 +2,17 @@ _base_ = [
     "./faster_rcnn_r50_fpn_1x_cityscapes.py",
 ]
 
+load_from = ""
+
 model = dict(
     roi_head=dict(
         bbox_roi_extractor=dict(
             roi_layer=dict(type="GpsaRoIPool", output_size=7, num_features=256),
         )
     )
+)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=8,
 )
