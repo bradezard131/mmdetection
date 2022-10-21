d2c6c2de79efdba19681163f937082d903bd58b7
Brad Ezard <bradley.ezard@postgrad.curtin.edu.au>
2022-10-21 09:19:49 +0800

    wip

--- configs/cityscapes/at_pool_faster_rcnn.py
+++ configs/cityscapes/at_pool_faster_rcnn.py
@@ -7,12 +7,14 @@ load_from = ""
 model = dict(
     roi_head=dict(
         bbox_roi_extractor=dict(
-            roi_layer=dict(type="GpsaRoIPool", output_size=7, num_features=256),
+            roi_layer=dict(
+                type="DeformAttnRoIPool", output_size=(7, 7), num_features=256
+            ),
         )
     )
 )
 
 data = dict(
-    samples_per_gpu=4,
+    samples_per_gpu=2,
     workers_per_gpu=8,
 )
